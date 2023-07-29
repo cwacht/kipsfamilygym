6788ac6353441bd5dd299e03bcf9acb92d488218
Chris Wachtman <cwachtman@gmail.com>
2023-07-28 22:45:26 -0400

    colab

--- src/content/links/enrichment/enrichment.11tydata.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  type: "Enrichment",
-  order: 1
-}
---
+++ src/content/links/family/family.11tydata.js
@@ -0,0 +1,5 @@
+module.exports = {
+  type: "Family Classes",
+  description: "For all family classes, one adult is expected to participate with the child.  Additional adults are welcome on an ad-hoc basis, depending on the size of the class.",
+  order: 1
+}
